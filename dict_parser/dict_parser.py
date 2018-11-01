d01571d41a5384de67846368f745b9b8bbe0d2bb
Joon <joonable2@gmail.com>
2018-11-01 10:48:24 +0900

    parsing synonym dicts

--- dict_parser/dict_parser.py
+++ dict_parser/dict_parser.py
@@ -1,5 +1,6 @@
 dir_path = './dict/'
 
+
 def parse_user_dict(read_file_name, write_file_name):
     # csv_reader = csv.reader()
     read_file = open(dir_path + read_file_name, 'r+')
@@ -12,45 +13,55 @@ def parse_user_dict(read_file_name, write_file_name):
 
         info = line.split(",")
         word = info[0]
-        offsets = info[1]
+        offsets = info[1].strip()
 
-        line = word + ' => '
+        line = word + " "
+
         pos = 0
-        if len(offsets) == 1:
-            line += word
-        else:
-            if offsets[0] == '0':
-                line += word
-                # pos += len(word)
+        # if len(offsets) == 1:
+        #     line += word + " "
+        # else:
+        #     if offsets[0] == '0':
+        #         line += word + " "
+        #         # pos += len(word)
 
-            for offset in offsets:
-                if offset == 'C':
+        for offset in offsets:
+            if offset in '123456789':
+                line += word[pos:pos+int(offset)]+" "
+                pos += int(offset)
+            else:
+                if offset == 'C' or offset == 'J':
                     pos += 1
-                    continue
                 elif offset == 'X':
                     line += word[pos]
                     pos += 1
-                    continue
                 elif offset == 'Y':
                     line = line[:-1]
-                    line += word[pos]+','
+                    line += word[pos] + ' '
                     pos += 1
-                    continue
+                elif offset == 'a' or offset == 'b':
+                    num = len(word)
+                    for _ in offsets:
+                        if _ == offset:
+                            continue
+                        num -= int(_)
+                    line += word[pos:pos + num] + " "
+                    pos += num
+                elif offset == 'B':
+                    line += word[pos:pos + 1] + " "
+                    pos += 1
+                # print(offset)
 
-                line += word[pos:pos+int(offset)]+","
-                pos += int(offset)
-
-        if line[-1] == ',':
+        if line[-1] == ' ':
             line = line[:-1]
-        print(line)
-        write_file.write(line)
+        # print(line)
+        write_file.write(line+'\n')
 
     read_file.close()
     write_file.close()
 
 
 def parse_synonym_dict(read_file_name, write_file_name):
-    # csv_reader = csv.reader()
     read_file = open(dir_path + read_file_name, 'r+')
     write_file = open(dir_path + write_file_name, 'w+')
 
@@ -62,11 +73,9 @@ def parse_synonym_dict(read_file_name, write_file_name):
         info = line.split("\t")
         word = info[1]
         synonyms = info[2]
+        line = word + "," + synonyms + " => " + word + "," + synonyms
 
-        line = synonyms + ' => ' + word
-
-        print(line)
-        write_file.write(line)
+        write_file.write(line+'\n')
 
     read_file.close()
     write_file.close()
