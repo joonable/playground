56a02e8ba6195236b75dce617960e4ae9ecac5a1
Joon <joonable2@gmail.com>
2018-12-08 12:12:43 +0900

    initial commit

---
+++ NN/nn/layers.py
@@ -0,0 +1,290 @@
+import numpy as np
+from nn.init import initialize
+
+class Layer:
+    """Base class for all neural network modules.
+    You must implement forward and backward method to inherit this class.
+    All the trainable parameters have to be stored in params and grads to be
+    handled by the optimizer.
+    """
+    def __init__(self):
+        self.params, self.grads = dict(), dict()
+
+    def forward(self, *input):
+        raise NotImplementedError
+        
+    def backward(self, *input):
+        raise NotImplementedError
+
+
+class Linear(Layer):
+    """Linear (fully-connected) layer.
+
+    Args:
+        - in_dims (int): Input dimension of linear layer.
+        - out_dims (int): Output dimension of linear layer.
+        - init_mode (str): Weight initalize method. See `nn.init.py`.
+          linear|normal|xavier|he are the possible options.
+        - init_scale (float): Weight initalize scale for the normal init way.
+          See `nn.init.py`.
+        
+    """
+    def __init__(self, in_dims, out_dims, init_mode="linear", init_scale=1e-3):
+        super().__init__()
+
+        self.params["w"] = initialize((in_dims, out_dims), init_mode, init_scale)
+        self.params["b"] = initialize(out_dims, "zero")
+    
+    def forward(self, x):
+        """Calculate forward propagation.
+
+        Returns:
+            - out (numpy.ndarray): Output feature of this layer.
+        """
+        ######################################################################
+        # TODO: Linear 레이어의 forward propagation 구현.
+        ######################################################################
+        out = np.add(np.dot(x, self.params["w"]), self.params["b"])
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        """Calculate backward propagation.
+
+        Args:
+            - dout (numpy.ndarray): Derivative of output `out` of this layer.
+        
+        Returns:
+            - dx (numpy.ndarray): Derivative of input `x` of this layer.
+        """
+        ######################################################################
+        # TODO: Linear 레이어의 backward propagation 구현.
+        ######################################################################
+        # output * (1.0-output)
+        db = self.params["b"] * dout.sum()
+        dw = self.params["w"] * dout.sum()
+        # dx =
+        # Layer Error
+        # Eo = (yHat-y) * relu_prime(Zo)
+        # Eo = dout * relu_prime(Zo)
+        # Eh = Eo * Wo * relu_prime(Zh)
+        #
+        # Cost derivative for weights
+        # dWo = Eo * H
+        # dWh = Eh * x
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        self.grads["w"] = dw
+        self.grads["b"] = db
+        return dx
+
+
+class ReLU(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        ######################################################################
+        # TODO: ReLU 레이어의 forward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        ######################################################################
+        # TODO: ReLU 레이어의 backward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        return dx
+
+
+class Sigmoid(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        ######################################################################
+        # TODO: Sigmoid 레이어의 forward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        ######################################################################
+        # TODO: Sigmoid 레이어의 backward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        return dx
+
+
+class Tanh(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        ######################################################################
+        # TODO: Tanh 레이어의 forward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        ######################################################################
+        # TODO: Tanh 레이어의 backward propagation 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        return dx
+
+
+class SoftmaxCELoss(Layer):
+    """Softmax and cross-entropy loss layer.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, y):
+        """Calculate both forward and backward propagation.
+        
+        Args:
+            - x (numpy.ndarray): Pre-softmax (score) matrix (or vector).
+            - y (numpy.ndarray): Label of the current data feature.
+
+        Returns:
+            - loss (float): Loss of current data.
+            - dx (numpy.ndarray): Derivative of pre-softmax matrix (or vector).
+        """
+        ######################################################################
+        # TODO: Softmax cross-entropy 레이어의 구현. 
+        #        
+        # NOTE: 이 메소드에서 forward/backward를 모두 수행하고, loss와 gradient (dx)를 
+        # 리턴해야 함.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return loss, dx
+    
+    
+class Conv2d(Layer):
+    """Convolution layer.
+
+    Args:
+        - in_dims (int): Input dimension of conv layer.
+        - out_dims (int): Output dimension of conv layer.
+        - ksize (int): Kernel size of conv layer.
+        - stride (int): Stride of conv layer.
+        - pad (int): Number of padding of conv layer.
+        - Other arguments are same as the Linear class.
+    """
+    def __init__(
+        self, 
+        in_dims, out_dims,
+        ksize, stride, pad,
+        init_mode="linear",
+        init_scale=1e-3
+    ):
+        super().__init__()
+        
+        self.params["w"] = initialize(
+            (out_dims, in_dims, ksize, ksize), 
+            init_mode, init_scale)
+        self.params["b"] = initialize(out_dims, "zero")
+        
+        self.in_dims = in_dims
+        self.out_dims = out_dims
+        self.ksize = ksize
+        self.stride = stride
+        self.pad = pad
+    
+    def forward(self, x):
+        ######################################################################
+        # TODO: Convolution 레이어의 forward propagation 구현.
+        #
+        # HINT: for-loop의 4-중첩으로 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        ######################################################################
+        # TODO: Convolution 레이어의 backward propagation 구현.
+        #
+        # HINT: for-loop의 4-중첩으로 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        self.grads["w"] = dw
+        self.grads["b"] = db
+        return dx
+    
+
+class MaxPool2d(Layer):
+    """Max pooling layer.
+
+    Args:
+        - ksize (int): Kernel size of maxpool layer.
+        - stride (int): Stride of maxpool layer.
+    """
+    def __init__(self, ksize, stride):
+        super().__init__()
+        
+        self.ksize = ksize
+        self.stride = stride
+        
+    def forward(self, x):
+        ######################################################################
+        # TODO: Max pooling 레이어의 forward propagation 구현.
+        #
+        # HINT: for-loop의 2-중첩으로 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        return out
+
+    def backward(self, dout):
+        ######################################################################
+        # TODO: Max pooling 레이어의 backward propagation 구현.
+        #
+        # HINT: for-loop의 4-중첩으로 구현.
+        ######################################################################
+
+        ######################################################################
+        #                          END OF YOUR CODE                          #
+        ######################################################################
+        self.grads["x"] = dx
+        return dx
